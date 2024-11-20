e8584dd9323ce78dee1b6540cac17debcaa4c313
alaalsalam <alaalsalam101@gmail.com>
2024-11-20 13:54:56 +0000

    update purchase invoice dashboard

--- custom_fst/hooks.py
+++ custom_fst/hooks.py
@@ -17,6 +17,12 @@ override_doctype_class = {
     }
 
 
+override_doctype_dashboards = {
+	"Purchase Invoice": "custom_fst.overrides.purchase_invoice_dashboard.get_dashboard",
+	# "Employee": "hrms.overrides.dashboard_overrides.get_dashboard_for_employee",
+ 
+	# "Task": "custom_fst.task.get_dashboard_data"
+}
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
@@ -190,9 +196,7 @@ override_doctype_class = {
 # each overriding function accepts a `data` argument;
 # generated from the base implementation of the doctype dashboard,
 # along with any modifications made in other Frappe apps
-# override_doctype_dashboards = {
-# 	"Task": "custom_fst.task.get_dashboard_data"
-# }
+
 
 # exempt linked doctypes from being automatically cancelled
 #
